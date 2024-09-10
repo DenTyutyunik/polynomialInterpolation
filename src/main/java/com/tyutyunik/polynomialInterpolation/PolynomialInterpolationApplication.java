75745b3dca3bb48f8201bab1b1e48d2af31edb08
DenTyutyunik <den.tyutyunik@gmail.com>
2024-09-10 18:42:59 +0400

    initial commit

---
+++ src/main/java/com/tyutyunik/polynomialInterpolation/PolynomialInterpolationApplication.java
@@ -0,0 +1,13 @@
+package com.tyutyunik.polynomialInterpolation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PolynomialInterpolationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PolynomialInterpolationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tyutyunik/polynomialInterpolation/PolynomialInterpolationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tyutyunik.polynomialInterpolation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PolynomialInterpolationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
